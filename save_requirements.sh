16c7d3e488d120d45e2aeed0cd538e616cb76941
samuraiamz <60890185+samuraiamz@users.noreply.github.com>
2026-08-12 11:44:31 +0100

    Create save_requirements.sh

---
+++ save_requirements.sh
@@ -0,0 +1,4 @@
+# Run this script to install the package locally
+pip freeze > requirements.txt
+
+# pip is used to save the packages installed into a file called requirements.txt in the top level of impyrial
